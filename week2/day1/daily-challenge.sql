05672b4b2d762d080b662ae0d566a7687db23b83
meryama-s <rafmeryama@gmail.com>
2025-09-23 22:47:15 +0100

    Update daily-challenge.sql

--- week2/day1/daily-challenge.sql
+++ week2/day1/daily-challenge.sql
@@ -1,8 +1,3 @@
--- ========================
--- DAILY CHALLENGE: ACTORS
--- ========================
-
--- 1) Create table actors
 DROP TABLE IF EXISTS actors;
 
 CREATE TABLE actors(
@@ -13,7 +8,6 @@ CREATE TABLE actors(
     number_oscars SMALLINT NOT NULL
 );
 
--- 2) Insert sample data
 INSERT INTO actors (first_name, last_name, birth_date, number_oscars)
 VALUES
 ('Matt', 'Damon', '1970-10-08', 5),
@@ -21,11 +15,9 @@ VALUES
 ('Angelina', 'Jolie', '1975-06-04', 1),
 ('Jennifer', 'Aniston', '1969-02-11', 0);
 
--- 3) Question 1: Count how many actors are in the table
 SELECT COUNT(*) AS total_actors
 FROM actors;
 
--- 4) Question 2: Try to add a new actor with some blank fields
--- This will cause an error because birth_date and number_oscars are NOT NULL
+
 INSERT INTO actors (first_name, last_name)
 VALUES ('Tom', 'Hanks');
